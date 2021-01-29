a77d8a9f7e9c94b5a8824efb5a03e5aed1fa655e
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2021-01-29 14:43:34 +0100

    07. feat: binding  model with ref

---
+++ src/pages/Register.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ count }}</h1>
+<input type="number" v-model="count">
+</template>
+
+<script>
+
+import {ref} from 'vue';
+
+export default {
+  name: "Register",
+
+  setup() {
+    const count = ref(0);
+
+    return {
+      count
+    }
+  }
+
+}
+</script>
