cb6b5f38195e54ab2ba09da6543d53b041b2bb02
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2021-03-02 23:32:36 +0100

    27. feat: create new product

---
+++ src/pages/products/ProductCreate.vue
@@ -0,0 +1,55 @@
+<template>
+  <form @click.prevent="submit">
+    <div class="mb-3">
+      <label>Titre</label>
+      <input v-model="data.title" class="form-control" name="title">
+    </div>
+    <div class="mb-3">
+      <label>Description</label>
+      <input v-model="data.description" class="form-control" name="description">
+    </div>
+    <div class="mb-3">
+      <label>Image</label>
+      <input v-model="data.image" class="form-control" name="image">
+    </div>
+    <div class="mb-3">
+      <label>Price</label>
+      <input v-model="data.price" type="number" class="form-control" name="price">
+    </div>
+    <button class="btn btn-outline-secondary">Save</button>
+  </form>
+</template>
+
+<script lang="ts">
+import {reactive} from 'vue'
+import axios from "axios";
+import {useRouter} from "vue-router";
+
+export default {
+  name: "productCreate",
+
+  setup() {
+    const router = useRouter();
+    const data = reactive({
+      title: ' ',
+      description: '',
+      image: '',
+      price: ''
+    });
+
+    const submit = async () => {
+      await axios.post('products', data)
+      await router.push('/products')
+    }
+
+    return {
+      data,
+      submit
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
